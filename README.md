92bb56a4700c399a99ed494373bd643437ce6e47
wyd <wyudi@qq.com>
2021-03-20 07:31:18 +0800

    javafx with jnativehook try some listener

--- README.md
+++ README.md
@@ -9,7 +9,7 @@ java system keyboard/mouse hook
 - system-hook(有用)
 
 - jnavitehook(有用)
-
+  
 - jintellitype(可能由于版本问题，无法使用)
 
 
@@ -20,3 +20,8 @@ system-hook不可跨平台，仅支持win
 
 jna本身功能强大，也可以实现keyboard hook。也可以跨平台，但是macos下的如何实现hook未实践。
 由于jna本身不是为了hook而实现的，其他功能丰富，但是实现hook略显麻烦。
+
+
+-- javafx 17 新增了Platform.isKeyLocked(KeyCode.CAPS)（must be one of: KeyCode.CAPS or KeyCode.NUM_LOCK. ）
+解决了swing toolkit加载后keyState返回值永不变的bug。
+本次新增了全局监听demo，hover 事件监听， focused 等事件。可查看demo。
---
+++ javafx-listening/JfxDispatchService.java
@@ -0,0 +1,45 @@
+//
+// Source code recreated from a .class file by IntelliJ IDEA
+// (powered by FernFlower decompiler)
+//
+
+package org.rainday.test.keyHook;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.AbstractExecutorService;
+import java.util.concurrent.TimeUnit;
+import javafx.application.Platform;
+
+public class JfxDispatchService extends AbstractExecutorService {
+    private boolean running = false;
+
+    public JfxDispatchService() {
+        this.running = true;
+    }
+
+    public void shutdown() {
+        this.running = false;
+    }
+
+    public List<Runnable> shutdownNow() {
+        this.running = false;
+        return new ArrayList(0);
+    }
+
+    public boolean isShutdown() {
+        return !this.running;
+    }
+
+    public boolean isTerminated() {
+        return !this.running;
+    }
+
+    public boolean awaitTermination(long var1, TimeUnit var3) throws InterruptedException {
+        return true;
+    }
+
+    public void execute(Runnable var1) {
+        Platform.runLater(var1);
+    }
+}
